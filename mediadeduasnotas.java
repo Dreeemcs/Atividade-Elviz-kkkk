a9b140f387c650f2a173ed99affa816b88e0aa1f
Dreeemcs <20251PGUA10010024@estudantes.ifpr.edu.br>
2026-04-03 11:56:55 -0300

    Teste

--- mediadeduasnotas.java
+++ mediadeduasnotas.java
@@ -8,12 +8,23 @@ public class mediadeduasnotas {
         double nota1 = scanner.nextDouble(); // Lê a primeira nota do usuário
         System.out.print("Digite a segunda nota: ");
         double nota2 = scanner.nextDouble(); // Lê a segunda nota do usuário
-        double media = (nota1 + nota2) / 2; //Calcula a média das duas notas
-        System.out.println("A média das notas é: " + media); //Exibe a média calculada
+        double media = calcularMedia(nota1, nota2); // Chama o método para calcular a média
+        System.out.println("A média das notas é: " + media); // Exibe a média calculada
+        scanner.close();
+        classificacao(media);
+    }
 
+    public static double calcularMedia(double nota1, double nota2) {
+        return (nota1 + nota2) / 2;
+    }
 
-    } public static void calcularMedia(double nota1, double nota2) {
-        double media = (nota1 + nota2) / 2; //Calcula a média das duas notas
-        System.out.println("A média das notas é: " + media); //Exibe a média calculada
+    public static void classificacao(double media) {
+        if (media >= 7) {
+            System.out.println("O aluno está aprovado.");
+         } else if (media >= 5) {
+            System.out.println("O aluno está em recuperação.");
+         } else {
+            System.out.println("O aluno está reprovado.");
+         }
     }
 }
