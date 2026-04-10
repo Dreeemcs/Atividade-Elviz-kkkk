680a48d124765bdba5ba64acc4c09d71574a30c6
Dreeemcs <20251PGUA10010024@estudantes.ifpr.edu.br>
2026-04-10 16:57:06 -0300

    orientado

--- Media.java
+++ Media.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
 
 public class Media {
-    public static void main(String[] args) {
-        Scanner Second = new Scanner(System.in);
-        System.out.print("Digite a primeira nota: ");
-        double nota = Second.nextDouble();
-        System.out.print("Digite a segunda nota: ");
-        double nota2 = Second.nextDouble();
-        System.out.print("Digite a terceira nota: ");
-        double nota3 = Second.nextDouble();
-        System.out.print("Digite a quarta nota: ");
-        double nota4 = Second.nextDouble();
-        double media = (nota + nota2 + nota3 + nota4) / 4;
-        System.out.println("A média é: " + media);
+    double nota1, nota2, nota3, nota4;
 
-       if (media >= 7) {
-            System.out.println("Aprovado");
-        } else if (media >= 5) {
-            System.out.println("Recuperação");
-        } else {
-            System.out.println("Reprovado");
-
-        }
-       }
+    public Media(double n1, double n2, double n3, double n4) {
+        this.nota1 = n1;
+        this.nota2 = n2;
+        this.nota3 = n3;
+        this.nota4 = n4;
     }
+
+    public double calcularMedia() {
+        return (nota1 + nota2 + nota3 + nota4) / 4;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a primeira nota: ");
+        double n1 = sc.nextDouble();
+        System.out.print("Digite a segunda nota: ");
+        double n2 = sc.nextDouble();
+        System.out.print("Digite a terceira nota: ");
+        double n3 = sc.nextDouble();
+        System.out.print("Digite a quarta nota: ");
+        double n4 = sc.nextDouble();
+
+        Media aluno = new Media(n1, n2, n3, n4);
+        System.out.println("A média é: " + aluno.calcularMedia());
+    }
+}
